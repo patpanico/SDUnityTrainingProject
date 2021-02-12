9135b7a3ec25da6e93e2f27efcd25186a725a1e8
sohum7 <57050073+sohum7@users.noreply.github.com>
2021-02-11 19:39:25 -0500

    Basic operations in lab
    - pour fluid from cylinder
    - rotate and move human/cube

---
+++ Assets/Scripts/Sohum/sphereGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphereGenerator : MonoBehaviour
+{
+    public float waitTime = 5;
+    public GameObject prefabType;
+    public int count;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(count > 0)
+        {
+            for(int i = 0; i<count; i++)
+            {
+                Instantiate(prefabType, new Vector3(0, 6, 0), Quaternion.identity);
+                StartCoroutine(coroutineWait());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator coroutineWait()
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
+}
