6c3d159a3d709568e3f6bba7fff9a93b4872e46c
sohum7 <57050073+sohum7@users.noreply.github.com>
2021-02-15 16:54:36 -0500

    Quick update to folder structure
    Updating folder structure

--- Assets/Scripts/Sohum/sphereGenerator.cs
+++ Assets/Scripts/Sohum/sphereGenerator.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class sphereGenerator : MonoBehaviour
 {
-    public float waitTime = 5;
-    public GameObject prefabType;
+    public float waitTime = 2;
+    public Transform prefabType;
     public int count;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        if(count > 0)
-        {
-            for(int i = 0; i<count; i++)
-            {
-                Instantiate(prefabType, new Vector3(0, 6, 0), Quaternion.identity);
-                StartCoroutine(coroutineWait());
-            }
-        }
+        // if(count > 0)
+        // {
+        //     for(int i = 0; i<count; i++)
+        //     {
+        //         Instantiate(prefabType, new Vector3(0, 6, 0), Quaternion.identity);
+        //         StartCoroutine(coroutineWait());
+        //     }
+        // }
     }
 
     // Update is called once per frame
