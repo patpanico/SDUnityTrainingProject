55b598dfbad686fab95a9be5b032e93f15294138
Patricio Panico <patpanico@hotmail.com>
2021-03-28 18:05:05 -0400

    Added Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void BiologyButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ChemistryButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
