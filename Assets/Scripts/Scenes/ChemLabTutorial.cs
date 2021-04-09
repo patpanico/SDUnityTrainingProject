0decca2c7c62da503759cadd2f2f0c700a70e0d5
Patricio Panico <patpanico@hotmail.com>
2021-04-09 00:15:03 -0400

    Added TutorialText UI

--- Assets/Scripts/Scenes/ChemLabTutorial.cs
+++ Assets/Scripts/Scenes/ChemLabTutorial.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChemLabTutorial : MonoBehaviour
 {
+    Text TutorialText;
 
-    
     void Start()
     {
-        
+        TutorialText = GameObject.Find("TutorialText").GetComponent<Text>();
     }
 
     void Update()
     {
         
     }
+
+
 }
