a2ae6ef1d4d18266ffec52901140a2f1434bb7aa
Patricio Panico <patpanico@hotmail.com>
2021-04-11 13:25:53 -0400

    Add TutorialPopup Object/Script

---
+++ Assets/Scripts/TutorialPopup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPopup : MonoBehaviour
+{
+    public GameObject CrosshairObj;
+    Text PopupText;
+
+    void Start()
+    {
+        PopupText = this.transform.Find("PopupText").GetComponent<Text>();
+    }
+
+    public void OpenPopup(string text)
+    {
+        PopupText.text = text;
+        this.gameObject.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        CrosshairObj.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void ClosePop()
+    {
+        this.gameObject.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        CrosshairObj.SetActive(true);
+        Time.timeScale = 1f;
+    }
+}
